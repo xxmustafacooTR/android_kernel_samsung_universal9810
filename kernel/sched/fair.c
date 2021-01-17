2302712738ebd237da1032b92591918fb24f225f
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-01-17 16:55:20 -0500

    sched: fair: Use Google's schedtune_margin implementation
    https://android.googlesource.com/kernel/msm/+/7a1b8ea81e64234cc2e9bff7634eb88d755d715b
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6596,8 +6596,10 @@ schedtune_margin(unsigned long capacity, unsigned long signal, long boost)
 	 * The obtained M could be used by the caller to "boost" S.
 	 */
 	if (boost >= 0) {
-		margin  = capacity - signal;
-		margin *= boost;
+		if (capacity > signal) {
+			margin  = capacity - signal;
+			margin *= boost;
+		}
 	} else {
 		margin = -signal * boost;
 	}
@@ -6627,7 +6629,7 @@ static inline long
 schedtune_task_margin(struct task_struct *p)
 {
 	int boost = schedtune_task_boost(p);
-	unsigned long util, capacity;
+	unsigned long util;
 
 	if (boost == 0)
 		return 0;
@@ -6635,7 +6637,7 @@ schedtune_task_margin(struct task_struct *p)
 	util = task_util_est(p);
 	capacity = capacity_orig_of(task_cpu(p));
 
-	return schedtune_margin(capacity, util, boost);
+	return schedtune_margin(SCHED_CAPACITY_SCALE, util, boost);
 }
 
 #else /* CONFIG_SCHED_TUNE */
