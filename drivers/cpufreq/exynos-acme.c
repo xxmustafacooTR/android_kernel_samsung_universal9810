1540449d65166c27d4738dd0917f5b1e57daff48
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-01-20 04:30:14 -0500

    exynos-acme: Default QoS for user

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -1112,6 +1112,10 @@ static __init int init_domain(struct exynos_cpufreq_domain *domain,
 		domain->max_usable_freq = min(domain->max_freq, val);
 #endif
 
+	/* Default QoS for user */
+	if (!of_property_read_u32(dn, "user-default-qos", &val))
+		domain->user_default_qos = val;
+
 	domain->boot_freq = cal_dfs_get_boot_freq(domain->cal_id);
 	domain->resume_freq = cal_dfs_get_resume_freq(domain->cal_id);
 
