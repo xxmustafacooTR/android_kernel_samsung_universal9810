b2d38e2c8b6dfb883e5389a848eb7cf9d4d36bf8
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-04-03 09:20:06 -0400

    gpu: mali: bv_r26p0: Change limit big cluster frequency only on high GPU util
    for different bus modes

--- drivers/gpu/arm/bv_r26p0/platform/exynos/gpu_pmqos.c
+++ drivers/gpu/arm/bv_r26p0/platform/exynos/gpu_pmqos.c
@@ -126,7 +126,15 @@ int gpu_pm_qos_command(struct exynos_context *platform, gpu_pmqos_state state)
 #endif
 		pm_qos_update_request(&exynos5_g3d_cpu_cluster0_min_qos, platform->table[platform->step].cpu_little_min_freq); // LITTLE Min request
 		if (!platform->boost_is_enabled) {
-			if (platform->env_data.utilization > 65)
+#ifdef CONFIG_PCIEASPM_PERFORMANCE
+			if (platform->env_data.utilization > 99)
+#else
+#ifdef CONFIG_PCIEASPM_BATTERY
+			if (platform->env_data.utilization > 5)
+#else
+			if (platform->env_data.utilization > 15)
+#endif
+#endif
 				pm_qos_update_request(&exynos5_g3d_cpu_cluster1_max_qos, platform->table[platform->step].cpu_big_max_freq);
 			else
 				pm_qos_update_request(&exynos5_g3d_cpu_cluster1_max_qos, PM_QOS_CLUSTER1_FREQ_MAX_DEFAULT_VALUE);
@@ -134,7 +142,15 @@ int gpu_pm_qos_command(struct exynos_context *platform, gpu_pmqos_state state)
 #if PM_QOS_CPU_CLUSTER_NUM == 3
 		pm_qos_update_request(&exynos5_g3d_cpu_cluster1_min_qos, platform->table[platform->step].cpu_middle_min_freq); /* MIDDLE Min request */
 		if (!platform->boost_is_enabled) {
-			if (platform->env_data.utilization > 65)
+#ifdef CONFIG_PCIEASPM_PERFORMANCE
+			if (platform->env_data.utilization > 99)
+#else
+#ifdef CONFIG_PCIEASPM_BATTERY
+			if (platform->env_data.utilization > 5)
+#else
+			if (platform->env_data.utilization > 15)
+#endif
+#endif
 				pm_qos_update_request(&exynos5_g3d_cpu_cluster1_max_qos, platform->table[platform->step].cpu_big_max_freq);
 			else
 				pm_qos_update_request(&exynos5_g3d_cpu_cluster1_max_qos, PM_QOS_CLUSTER1_FREQ_MAX_DEFAULT_VALUE);
