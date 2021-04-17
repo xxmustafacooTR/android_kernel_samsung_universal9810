360ad716678e484169c7e12f93d3b7df7202491c
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-04-17 19:32:48 -0400

    BuildScript and .gitignore: hide and delete builded dtb

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf drivers/gator_5.27/gator_src_md5.h arch/arm64/boot/dts/exynos/*dtb*
+rm -rf drivers/gator_5.27/gator_src_md5.h scripts/dtbtool_exynos/dtbtool arch/arm64/boot/dtb.img arch/arm64/boot/dts/exynos/*dtb*
 make clean
 make mrproper
