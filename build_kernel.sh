0df38a6155b7914e65585ca7de2e03bf89e7a823
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-04-24 15:25:06 -0400

    BuildScript: change toolchain to Cruel 10.3

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/kali/Android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/kali/Android/Toolchains/aarch64-cruel-elf/bin/aarch64-cruel-elf-
 export CROSS_COMPILE_ARM32=/home/kali/Android/Toolchains/gcc-9.2-arm-none-eabi/bin/arm-none-eabi-
 export CC=/home/kali/Android/Toolchains/proton-clang/bin/clang
 export ANDROID_MAJOR_VERSION=r
