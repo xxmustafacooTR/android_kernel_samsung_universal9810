799c649907e5ad81425354e291955bf46b44d8d0
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-05-01 20:52:23 -0400

    BuildScript: change toolchains to xxmustafacooTR Dev Builts

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/kali/Android/Toolchains/gcc-prebuilt-elf-toolchains/aarch64-milouk-elf/bin/aarch64-milouk-elf-
-export CROSS_COMPILE_ARM32=/home/kali/Android/Toolchains/gcc-9.2-arm-none-eabi/bin/arm-none-eabi-
+export CROSS_COMPILE=/home/kali/Android/Toolchains/aarch64-xxmustafacooTR-elf/bin/aarch64-xxmustafacooTR-elf-
+export CROSS_COMPILE_ARM32=/home/kali/Android/Toolchains/arm-xxmustafacooTR-eabi/bin/arm-xxmustafacooTR-eabi-
 export CC=/home/kali/Android/Toolchains/proton-clang/bin/clang
 export ANDROID_MAJOR_VERSION=r
 
