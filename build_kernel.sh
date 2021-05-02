195c976ff7d84a482ceeec6908a40b23d3907cf4
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-05-02 13:26:21 -0400

    BuildScript: add a fd limit reminder

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,5 +6,6 @@ export CROSS_COMPILE_ARM32=/home/kali/Android/Toolchains/arm-xxmustafacooTR-eabi
 export CC=/home/kali/Android/Toolchains/proton-clang/bin/clang
 export ANDROID_MAJOR_VERSION=r
 
+echo "Write 'ulimit -n unlimited' to current terminal"
 make exynos9810-star2lte_defconfig
 make -j8
