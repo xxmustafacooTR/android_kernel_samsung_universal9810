f4a8f20c38b694a598b0d05fc0af5382997aa0a4
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2021-06-10 05:04:13 -0400

    supress mcpu and march conflict we already defined them

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/home/kali/Android/Toolchains/aarch64-xxmustafacooTR-elf/bin/aarch64-xxmustafacooTR-elf-
+export CROSS_COMPILE=/home/kali/Android/Toolchains/aarch64-cruel-elf/bin/aarch64-cruel-elf-
 export CROSS_COMPILE_ARM32=/home/kali/Android/Toolchains/arm-xxmustafacooTR-eabi/bin/arm-xxmustafacooTR-eabi-
 export CC=/home/kali/Android/Toolchains/proton-clang/bin/clang
 export ANDROID_MAJOR_VERSION=r
